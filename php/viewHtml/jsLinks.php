0a9636d6e99794b405e990c82745d1df7c98655d
diehercasvan <diehercasvan@gmail.com>
2026-01-19 19:22:18 -0500

    New changes

--- php/viewHtml/jsLinks.php
+++
@@ -1,6 +0,0 @@
-  <script src="https://rdl.dendrite.com.co/js/functionsSite.js"></script>
-  <script src="https://rdl.dendrite.com.co/js/Storage.js"></script>
-  <script src="https://rdl.dendrite.com.co/pages/login/js/login.js"></script> 
-  <script src="https://rdl.dendrite.com.co/js/table-filter.js"></script> 
-  <script src="https://rdl.dendrite.com.co/js/sweetalert2.all.min.js"></script> 
-  
