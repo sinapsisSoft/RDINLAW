8caf0e47c51c437c0f13bb5529bb8fb41ba8549f
diehercasvan <diehercasvan@gmail.com>
2026-05-19 13:32:21 -0500

    Add manuals dropdown to navbar and HTML fixes
    Introduce a 'Manuales' dropdown in pages/navbar/navbar.php with a lord-icon button and direct links to several PDF manuals, adjust notification bell markup and add small layout spacing. Also update logout and header text to use HTML entities for accents and make minor HTML formatting tweaks in pages/dashboard/index.php (self-closing meta spacing and small whitespace changes).

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../../config/SessionManager.php';
 require_once '../../config/config.php';
+
 // Iniciar sesión
 SessionManager::start();
 $var_session = SessionManager::get(SESSION_KEY);
@@ -15,7 +16,7 @@ if ($var_session === null) {
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
+  <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
--- pages/navbar/navbar.php
+++ pages/navbar/navbar.php
@@ -1,3 +1,5 @@
+
+
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
   <!-- Navbar Brand-->
   <a class="navbar-brand ps-3" href="#"><img src="../assets/startbootstrap-sb-admin/assets/img/logos/logo-slide_bar.png" class="img w-50"></a>
@@ -17,9 +19,36 @@
   <!-- Navbar Search-->
   <div class="d-md-inline-block form-inline ms-auto ">
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-3">
-      <li class="nav-item dropdown">
+      <li class="nav-item dropdown" style="margin-left: 10px;">
         <a class="nav-link " id="navbarDropdown" style="padding:inherit" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           <button type="button" class="btn btn-primary position-relative">
+            <lord-icon
+              src="https://cdn.lordicon.com/yhtmwrae.json"
+              trigger="hover"
+              trigger="hover"
+              stroke="light"
+              state="hover-pinch"
+              href="index.html"
+              colors="primary:#ffffff,secondary:#b4b4b4"
+              style="width:20px;height:20px;">
+            </lord-icon>
+           
+          </button>
+        </a>
+        <div id="menu-notifications" class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#"><strong>Manuales </strong></a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" style="font-size: 1em;" href="https://rdl.dendrite.com.co/documents/manuals/Manual%20-%20Panel_de_Control_Dendrite.pdf" target="_blank">Panel de Control</a>
+          <a class="dropdown-item" style="font-size: 1em;" href="https://rdl.dendrite.com.co/documents/manuals/Manual%20-%20Modulo_de_Expedientes_Dendrite.pdf" target="_blank">Expedientes</a>
+          <a class="dropdown-item" style="font-size: 1em;" href="https://rdl.dendrite.com.co/documents/manuals/Manual%20-%20Modulo_de_Solicitudes_Dendrite.pdf" target="_blank">Solicitudes</a>
+          <a class="dropdown-item" style="font-size: 1em;" href="https://rdl.dendrite.com.co/documents/manuals/Manual%20-%20Modulo_de_Calendario_y_Eventos_Dendrite.pdf" target="_blank">Informes</a>
+          <a class="dropdown-item" style="font-size: 1em;" href="https://rdl.dendrite.com.co/documents/manuals/Manual%20-%20Modulo_de_Informes_Dendrite.pdf" target="_blank">Calendario</a>
+
+        </div>
+      </li>
+      <li class="nav-item dropdown " style="margin-left: 10px;">
+        <a class="nav-link " id="navbarDropdown" style="padding:inherit" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <button type="button" class="btn btn-warning position-relative">
             <lord-icon class="lord-icon"
               src="../assets/startbootstrap-sb-admin/assets/img/icons/json/system-regular-46-notification-bell-hover-bell.json"
               trigger="hover"
@@ -35,7 +64,8 @@
           </button>
         </a>
         <div id="menu-notifications" class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-          <h6 class="dropdown-header">Próximos eventos</h6>
+          <h6 class="dropdown-header">Pr&#243ximos eventos</h6>
+
         </div>
       </li>
     </ul>
@@ -58,7 +88,7 @@
         <li>
           <hr class="dropdown-divider" />
         </li>
-        <li><a class="dropdown-item " style="cursor:pointer" id="loginbtn" onclick="closeSession()">Cerrar sesión</a></li>
+        <li><a class="dropdown-item " style="cursor:pointer" id="loginbtn" onclick="closeSession()">Cerrar sesi&#243n</a></li>
       </ul>
     </li>
   </ul>
